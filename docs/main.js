b1c2e9ea169d75052db925a0e06cadc846305263
turbographics2000 <turbographics2000@yahoo.co.jp>
2018-07-26 13:49:52 +0900

    Create main.js

---
+++ docs/main.js
@@ -0,0 +1,30 @@
+const myId = (new MediaStream).id;
+console.log(`myId:${myId}`);
+let stream = null;
+function appendVideo(stream) {
+    const video = document.createElement('video');
+    video.srcObject = stream;
+    document.body.appendChild(video);
+    video.play();
+}
+const constraints = {
+    video: true,
+    // audio: true // オーディオを追加すると期待する挙動となる
+};
+navigator.mediaDevices.getUserMedia(constraints).then(stream => {
+    console.log(`streamId:${stream.id}`);
+    appendVideo(stream);
+    const peer = new Peer(myId, {
+        key: 'a5e6294b-f6ef-4edf-93c6-161f92ae5e8e' // 1時間映像のみ送受信 (2人接続して、2人目が1人目の映像が表示されていない状態)
+    });
+    peer.on('open', id => {
+        myIdDisp.textContent = id;
+        const room = peer.joinRoom('hoge_fuga_piyo_sfu', { mode: 'sfu', stream });
+        room.on('stream', stream => {
+            console.log(`room on stream peerId:${stream.peerId}`);
+            appendVideo(stream);
+        });
+    });
+}).catch(err => {
+    console.error(err);
+});
